b8bf5a8d3f1fbef4dcc41195ced1fe1282b40498
licpiter <mauro.mansilla@gmail.com>
2021-09-17 17:24:36 -0300

    separate class

--- CrudSucursal/src/main/java/ar/com/mmansilla/challenge/model/PuntoDeRetiro.java
+++ CrudSucursal/src/main/java/ar/com/mmansilla/challenge/model/PuntoDeRetiro.java
@@ -2,33 +2,47 @@ package ar.com.mmansilla.challenge.model;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
 @Entity
-@EqualsAndHashCode(callSuper = true)
 @Table(name="punto_retiro")
-public class PuntoDeRetiro extends Nodo implements Serializable {
+public class PuntoDeRetiro implements Serializable {
     private static final long serialVersionUID = 1L;
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Id
+    private double latitud;
+    private double longitud;
     private long capacidad;
 
     public PuntoDeRetiro() {
-        super(TipoNodo.PUNTO_RETIRO);
+        this.latitud = 0.0;
+        this.longitud = 0.0;
         this.capacidad = 0;
     }
 
     public PuntoDeRetiro(long capacidad) {
-        super(TipoNodo.PUNTO_RETIRO);
+        this.latitud = 0.0;
+        this.longitud = 0.0;
         this.capacidad = capacidad;
     }
     
     public PuntoDeRetiro(long capacidad, double latitud, double longitud) {
-        super(TipoNodo.PUNTO_RETIRO, latitud, longitud);
+        this.latitud = latitud;
+        this.longitud = longitud;
         this.capacidad = capacidad;
     }
 
+    public PuntoDeRetiro(double latitud, double longitud) {
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.capacidad = 0;
+    }
 }
--- CrudSucursal/src/main/java/ar/com/mmansilla/challenge/model/Sucursal.java
+++ CrudSucursal/src/main/java/ar/com/mmansilla/challenge/model/Sucursal.java
@@ -2,39 +2,54 @@ package ar.com.mmansilla.challenge.model;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
 @Entity
-@EqualsAndHashCode(callSuper = true)
 @Table(name="sucursal")
-public class Sucursal extends Nodo implements Serializable {
+public class Sucursal implements Serializable {
     
     private static final long serialVersionUID = 1L;
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Id
+    private double latitud;
+    private double longitud;
     private String direccion;
     private String horario_atencion;
 
     public Sucursal() {
-        super(TipoNodo.SUCURSAL);
+        this.latitud = 0.0;
+        this.longitud = 0.0;
         this.direccion = "SIN DATOS";
         this.horario_atencion = "SIN DATOS";
     }
     
     public Sucursal(String direccion, String horario_atencion) {
-        super(TipoNodo.SUCURSAL);
+        this.latitud = 0.0;
+        this.longitud = 0.0;
         this.direccion = direccion;
         this.horario_atencion = horario_atencion;
     }
     
-    public Sucursal(String direccion, String horario_atencion, double latitud, double longitud) {
-        super(TipoNodo.SUCURSAL, latitud, longitud);
+    public Sucursal(double latitud, double longitud, String direccion, String horario_atencion) {
+        this.latitud = latitud;
+        this.longitud = longitud;
         this.direccion = direccion;
         this.horario_atencion = horario_atencion;
     }
 
+    public Sucursal(double latitud, double longitud) {
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.direccion = "SIN DATOS";
+        this.horario_atencion = "SIN DATOS";
+    }
     
 }
